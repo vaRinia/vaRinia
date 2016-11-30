3d5eda3a6aca134b092872f67846b536c1cd9d1c
vaRinia <varinia.ep@gmail.com>
2016-11-29 22:01:02 -0600

    avance

---
+++ js/js.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".boton").click(function(){
+        var section = $(this).data("section");
+        var position = $("body").scrollTop();
+        $("body").animate({
+           scrollTop: $(section).offset().top - 50
+            }, 1300);    
+    });
+    $("#logo").hover(function(){
+    	console.log("listo1");
+    	$(".navbar-brand").html("<img alt='Logo vaRi_' src='img/logoNav2.png' class='img-responsive'>")}, function() {
+    		console.log("listo2");
+    		$(".navbar-brand").html("<img alt='logo' src='img/logoNav.png' class='img-responsive'>");
+    });
+    $("#logo").click(function(){
+        console.log("ahi va");
+        $("body").animate({
+           scrollTop: $("#inicio").offset().top - 150
+            }, 1500);    
+        
+    });
+});
